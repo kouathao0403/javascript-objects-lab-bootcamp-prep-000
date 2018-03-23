285631443a101a6b17727ebbe380b3387bd56d34
Koua Thao <kouathao09@gmail.com>
2018-03-23 02:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value;
+  object[key] = value;
 }
